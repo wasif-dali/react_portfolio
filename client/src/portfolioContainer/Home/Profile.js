c7d44f1be8acd2ecbd9cc2c440edf89b93ca0a8f
Wasif Ali <you@example.com>
2023-10-19 21:08:50 +0530

    name and info

--- client/src/portfolioContainer/Home/Profile.js
+++ client/src/portfolioContainer/Home/Profile.js
@@ -1,29 +1,60 @@
-import React from 'react'
-
+import React from "react";
+import Typical from "react-typical";
 export default function Profile() {
   return (
-    <div className='profile-container'>
-        <div className='profile-parent'>
-             <div className='profile-details'>
-                   <div className='colz'>
-                      <a href='https://www.facebook.com/wasifdaliali'>
-                         <i className='fa fa-facebook-square'></i>
-                      </a>
-                      <a href='https://www.instagram.com/wasif_aliali/'>
-                        <i className='fa fa-instagram-square'></i>
-                      </a>
-                      <a href='https://github.com/wasif-dali'>
-                        <i className='fa fa-github-square'></i>
-                      </a>
-                      <a href='https://www.threads.net/@wasif_aliali'>
-                        <i className='fa fa-threds-square'></i>
-                      </a>
-                      <a href='https://twitter.com/wasifk991'>
-                        <i className='fa fa-twitter-square'></i>
-                      </a>
-                   </div>
-            </div>
-        </div>
-     </div>
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="https://www.facebook.com/wasifdaliali">
+              <i className="fa fa-facebook-square"></i>
+            </a>
+            <a href="https://www.instagram.com/wasif_aliali">
+              <i className="fa fa-instagram"></i>
+            </a>
+            <a href="https://github.com/wasif-dali">
+              <i className="fa fa-github-square"></i>
+            </a>
+
+            <a href="https://twitter.com/wasifk991">
+              <i className="fa fa-twitter"></i>
+            </a>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {""}
+              Hello,I'M <span className="highlighted-text">Wasif Ali</span>
+            </span>
+          </div>
+        <div className='profile-details-role'>
+          <span className='primary-text'>
+            <h1>
+                  {""}
+                  <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Ethusiastic Dev🔴",
+                    1000,
+                    "Full Stack Developer💻",
+                    
+                    1000,
+                    "MERN Stack Dev🤓",
+                    
+                    1000,
+                    "Cross Platform🌐",
+                    
+                    1000,
+                    "React/React Native Dev📱",
+                    
+                    1000,
+                  ]}
+            />
+                  </h1>
+          </span>
+      </div>
+    </div>
+    </div>
+    </div>
+    
   );
 }
